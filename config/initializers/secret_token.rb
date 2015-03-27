2272c958d35c226ea800ae6d6b00e9af016c3b3c
JasonY1 <jasonh.yi@gmail.com>
2015-03-27 16:15:35 -0700

    active_model_serializers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+MyBlog::Application.config.secret_token = '92bde448ceb4fbc9a18753e9ced6f7e98bcf3403b336a1015c091359500bfe38e31f268084e74841128a64b3364ee9c73b91d0b92e074468507bd98bf21977a5'
